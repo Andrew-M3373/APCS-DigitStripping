7e7eaf2d7d8aedf8988b0d2a5f745289cf80e803
andrew.markel <andrew.markel@MU-C1-01.mullen.pvt.k12.co.us>
2021-08-26 12:03:51 -0600

    Initial commit

---
+++ Digit Stripping/src/DigitStripping.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class DigitStripping {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter a whole number and we will print it backward.");
+		
+		Scanner userInput = new Scanner(System.in);
+		int number = userInput.nextInt();
+		System.out.print("\nYour number printed backwards is: ");
+		int digit = number;
+		
+		while (number > 0)
+		{
+			digit = number % 10;
+			System.out.print(digit);
+			number  /= 10;
+		}
+		
+	}
+
+}
